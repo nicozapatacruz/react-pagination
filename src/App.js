d87bde791eb1e5f735ad2e94443ec79bb0091250
nicozapatacruz <nicozapatacruz@gmail.com>
2023-02-27 21:11:50 -0500

    Uploading Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+const API_URL = "https://api.instantwebtools.net/v1/passenger";
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetch(`${API_URL}?page=${page}&size=10`)
+      .then((response) => response.json())
+      .then((fetchData) => {
+        setData(fetchData);
+        setIsLoading(false);
+      });
+  }, [page]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Passengers</h1>
+      <div>
+        {data.data.map((passenger, i) => {
+          return (
+            <ul key={i}>
+              <li>Passenger Name: {passenger.name}</li>
+              <li>Number of trips: {passenger.trips}</li>
+            </ul>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={() => setPage(1)}>First Page</button>
+        <button onClick={() => (page > 1 ? setPage(page - 1) : null)}>Previous Page</button>
+        <button onClick={() => (page < 10 ? setPage(page + 1) : null)}>Next Page</button>
+        <button onClick={() => setPage(10)}>Last Page</button>
+      </div>
     </div>
   );
 }
